807d78c9d3772663e9caee4b60549d62cbb7315d
X4Ker <32191102+X4Ker@users.noreply.github.com>
2017-10-13 12:55:45 +0300

    Update 0.0.3.cpp

--- 0.0.3.cpp
+++ 0.0.3.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main() {
 	int a[10], b[10], c[20];
-	bool q = true;
+	bool q = true; p = true;
 	for (int i = 0; i < 10; i++) {
 		cin >> a[i];
 	}
 	for (int i = 0; i < 10; i++) {
 		cin >> b[i];
 	}
-  for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
 		if (a[i] => a[i + 1]) {
 			q = false;
 		}
-  }
-    for (int i = 0; i < 9; i++) {
+       }
+        for (int i = 0; i < 9; i++) {
 		if (b[i] => b[i + 1]) {
 			q = false;
 		}
